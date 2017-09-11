78373fe0799c1c7b5834d6c358a2e62fc8d5e640
Cezary Olejarczyk <colejarczyk@divante.pl>
2017-09-11 22:02:58 +0200

    Store username and show list

--- src/app.php
+++ src/app.php
@@ -5,16 +5,26 @@ use Silex\Provider\AssetServiceProvider;
 use Silex\Provider\TwigServiceProvider;
 use Silex\Provider\ServiceControllerServiceProvider;
 use Silex\Provider\HttpFragmentServiceProvider;
+use Silex\Provider\SessionServiceProvider;
 
 $app = new Application();
 $app->register(new ServiceControllerServiceProvider());
 $app->register(new AssetServiceProvider());
 $app->register(new TwigServiceProvider());
 $app->register(new HttpFragmentServiceProvider());
+$app->register(new SessionServiceProvider());
 $app['twig'] = $app->extend('twig', function ($twig, $app) {
     // add custom globals, filters, tags, ...
 
     return $twig;
 });
+
+// register sqlite
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options' => array(
+        'driver'   => 'pdo_sqlite',
+        'path'     => __DIR__.'/../db/app.db',
+    ),
+));
 
 return $app;
--- src/controllers.php
+++ src/controllers.php
@@ -1,5 +1,6 @@
 <?php
 
+use Doctrine\DBAL\Connection;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Silex\Application;
@@ -12,7 +13,6 @@ use Payum\Core\Request\GetHumanStatus;
 
 //Request::setTrustedProxies(array('127.0.0.1'));
 
-
 /**
  * @return Payum
  */
@@ -21,32 +21,46 @@ function getPayum()
     /** @var Payum $payum */
     return (new PayumBuilder())
         ->addDefaultStorages()
-        ->addGateway('aGateway', [
-            'factory' => 'paypal_express_checkout',
-            'username'  => 't.jurczak-facilitator_api1.gmail.com',
-            'password'  => '8X2CF9PG5K3NH7SS',
-            'signature' => 'AFcWxV21C7fd0v3bYYYRCpSSRl31A34GPHs4v0YNoWZQOXG2ec3X4ij1',
-            'sandbox'   => true,
-        ])
-        ->setGenericTokenFactoryPaths([
-            'capture' => 'capture',
-            'notify' => 'notify.php',
-            'authorize' => 'authorize.php',
-            'refund' => 'refund.php',
-            'payout' => 'payout.php',
-        ])
-        ->getPayum()
-    ;
+        ->addGateway(
+            'aGateway',
+            [
+                'factory'   => 'paypal_express_checkout',
+                'username'  => 't.jurczak-facilitator_api1.gmail.com',
+                'password'  => '8X2CF9PG5K3NH7SS',
+                'signature' => 'AFcWxV21C7fd0v3bYYYRCpSSRl31A34GPHs4v0YNoWZQOXG2ec3X4ij1',
+                'sandbox'   => true,
+            ]
+        )
+        ->setGenericTokenFactoryPaths(
+            [
+                'capture'   => 'capture',
+                'notify'    => 'notify.php',
+                'authorize' => 'authorize.php',
+                'refund'    => 'refund.php',
+                'payout'    => 'payout.php',
+            ]
+        )
+        ->getPayum();
 }
 
 /** @var Application $app */
 
 // main page
-$app->get('/', function () use ($app) {
-    return $app['twig']->render('index.html.twig', []);
-})->bind('homepage');
+$app->get(
+    '/',
+    function () use ($app) {
+        return $app['twig']->render('index.html.twig', []);
+    }
+)->bind('homepage');
 
-$app->get('/pay', function () use ($app) {
+$app->post('/pay', function (Request $request) use ($app) {
+    $username = $request->get('username', null);
+    $username = $app->escape($username);
+    if (empty($username) || $username == null) {
+        $app['session']->getFlashBag()->add('warning', '<b>Oops!</b> You forgot username!');
+        return $app->redirect("/");
+    }
+
     $paymentClass = Payment::class;
 
     /** @var Payum $payum */
@@ -57,105 +71,94 @@ $app->get('/pay', function () use ($app) {
     /** @var \Payum\Core\Payum $payum */
     $storage = $payum->getStorage($paymentClass);
 
+    /** @var Payment $payment */
     $payment = $storage->create();
     $payment->setNumber(uniqid());
     $payment->setCurrencyCode('EUR');
-    $payment->setTotalAmount(123); // 1.23 EUR
-    $payment->setDescription('A description');
-    $payment->setClientId('anId');
-    $payment->setClientEmail('foo@example.com');
-
-    $payment->setDetails(array(
-        // put here any fields in a gateway format.
-        // for example if you use Paypal ExpressCheckout you can define a description of the first item:
-        // 'L_PAYMENTREQUEST_0_DESC0' => 'A desc',
-    ));
+    $payment->setTotalAmount(100); // 1.00 EUR
+    $payment->setDescription($username);
 
     $storage->update($payment);
 
     $captureToken = $payum->getTokenFactory()->createCaptureToken($gatewayName, $payment, 'done');
 
-    header("Location: ".$captureToken->getTargetUrl());
-    die();
-
+    return $app->redirect($captureToken->getTargetUrl());
 })->bind('pay');
 
 $app->get('/pay/capture', function () use ($app) {
+        /** @var Payum $payum */
+        $payum = getPayum();
 
-    $paymentClass = Payment::class;
+        $token   = $payum->getHttpRequestVerifier()->verify($_REQUEST);
+        $gateway = $payum->getGateway($token->getGatewayName());
 
-    /** @var Payum $payum */
-    $payum = getPayum();
+        /** @var \Payum\Core\GatewayInterface $gateway */
+        if ($reply = $gateway->execute(new Capture($token), true)) {
+            if ($reply instanceof HttpRedirect) {
+                return $app->redirect($reply->getUrl());
+            }
 
-    $token = $payum->getHttpRequestVerifier()->verify($_REQUEST);
-    $gateway = $payum->getGateway($token->getGatewayName());
-
-    /** @var \Payum\Core\GatewayInterface $gateway */
-    if ($reply = $gateway->execute(new Capture($token), true)) {
-        if ($reply instanceof HttpRedirect) {
-            header("Location: ".$reply->getUrl());
-            die();
+            throw new \LogicException('Unsupported reply', null, $reply);
         }
 
-        throw new \LogicException('Unsupported reply', null, $reply);
+        /** @var \Payum\Core\Payum $payum */
+        $payum->getHttpRequestVerifier()->invalidate($token);
+
+        return $app->redirect($token->getAfterUrl());
     }
+)->bind('pay/capture');
 
-    /** @var \Payum\Core\Payum $payum */
-    $payum->getHttpRequestVerifier()->invalidate($token);
+$app->get(
+    '/pay/done',
+    function () use ($app) {
 
-    header("Location: ".$token->getAfterUrl());
-    die();
+        /** @var Payum $payum */
+        $payum = getPayum();
 
-})->bind('pay/capture');
+        /** @var \Payum\Core\Payum $payum */
+        try {
+            $token   = $payum->getHttpRequestVerifier()->verify($_REQUEST);
+        } catch (\Exception $e) {
+            return $app->redirect('/');
+        }
 
-$app->get('/pay/done', function () use ($app) {
+        $gateway = $payum->getGateway($token->getGatewayName());
 
-    /** @var Payum $payum */
-    $payum = getPayum();
+        $payum->getHttpRequestVerifier()->invalidate($token);
 
-    /** @var \Payum\Core\Payum $payum */
-    $token = $payum->getHttpRequestVerifier()->verify($_REQUEST);
-    $gateway = $payum->getGateway($token->getGatewayName());
+        // or Payum can fetch the model for you while executing a request (Preferred).
+        $gateway->execute($status = new GetHumanStatus($token));
+        /** @var Payment $payment */
+        $payment = $status->getFirstModel();
 
-// you can invalidate the token. The url could not be requested any more.
-// $payum->getHttpRequestVerifier()->invalidate($token);
+        $username = $payment->getDescription();
+        /** @var Connection $db */
+        $db = $app['db'];
 
-// Once you have token you can get the model from the storage directly.
-//$identity = $token->getDetails();
-//$payment = $payum->getStorage($identity->getClass())->find($identity);
+        $db->insert('users', ['username' => $app->escape($username), 'createdAt' => time()]);
 
-// or Payum can fetch the model for you while executing a request (Preferred).
-    $gateway->execute($status = new GetHumanStatus($token));
-    $payment = $status->getFirstModel();
+        $myId = $db->fetchAssoc('SELECT id FROM users where username = ?', [$username]);
+        $users = $db->fetchAll('SELECT * FROM users ORDER BY ID ASC');
 
-    $data = json_encode([
-        'status' => $status->getValue(),
-        'order' => [
-            'total_amount' => $payment->getTotalAmount(),
-            'currency_code' => $payment->getCurrencyCode(),
-            'details' => $payment->getDetails(),
-        ],
-    ]);
-
-    return $app['twig']->render('list.html.twig', ['data' => $data]);
-
-})->bind('pay/done');
-
-
+        return $app['twig']->render('list.html.twig', ['myId' => $myId['id'], 'users' => $users]);
+    }
+)->bind('pay/done');
 
 // error handler
-$app->error(function (\Exception $e, Request $request, $code) use ($app) {
-    if ($app['debug']) {
-        return;
+$app->error(
+    function (\Exception $e, Request $request, $code) use ($app) {
+        if ($app['debug']) {
+            return;
+        }
+
+        // 404.html, or 40x.html, or 4xx.html, or error.html
+        $templates = [
+            'errors/'.$code.'.html.twig',
+            'errors/'.substr($code, 0, 2).'x.html.twig',
+            'errors/'.substr($code, 0, 1).'xx.html.twig',
+            'errors/default.html.twig',
+        ];
+
+        return new Response($app['twig']->resolveTemplate($templates)->render(['code' => $code]), $code);
     }
-
-    // 404.html, or 40x.html, or 4xx.html, or error.html
-    $templates = array(
-        'errors/'.$code.'.html.twig',
-        'errors/'.substr($code, 0, 2).'x.html.twig',
-        'errors/'.substr($code, 0, 1).'xx.html.twig',
-        'errors/default.html.twig',
-    );
-
-    return new Response($app['twig']->resolveTemplate($templates)->render(array('code' => $code)), $code);
-});
+);
